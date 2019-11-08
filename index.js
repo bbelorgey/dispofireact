76514ef007af424703331721bacf5e2bff9646ad
bbelorgey <b.belorgey@dispofi.fr>
2019-11-08 14:12:00 +0100

    affichage du port

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const buildDir = path.resolve(__dirname, '../build');
 
 app.use(morgan(':date[clf] :method :url :req[header] :status :response-time ms - :res[content-length]'));
 
-console.log("Express server listening on port %d", app.address().port)
+console.log("Express server listening on port %d")
 
 app.use(express.static(buildDir));
 
-app.get('*', (req, res) => res.sendFile(`${buildDir}/index.html`));
+app.get('*', (req, res) => {
+  console.log('REQ : ', req);
+  res.sendFile(`${buildDir}/index.html`);
+});
 
 app.listen(process.env.PORT || 5010);
 
